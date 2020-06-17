94798e3bceeb3d69fe711d342209b261ff16018d
teresam856 <tere486martinez@gmail.com>
2020-06-17 03:26:50 -0700

    button is now appending placeholder text

--- intro_app/app.js
+++ intro_app/app.js
@@ -2,7 +2,12 @@ console.log("I am connected")
 
 var main = function () {
     "use strict";
-    window.alert("Hello Teresa!");
-}
+    console.log("I will begin to execute the main function")
+    $(".userItem button").on("click", function (event) {
+        console.log("I am adding an new Item");
+        $(".userItems").append("<p> placeholder text for new Item</p>");
+    });
+
+};
 
 $(document).ready(main);
