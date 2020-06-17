978f1501bb5822619143f130381c40662c22e84b
teresam856 <tere486martinez@gmail.com>
2020-06-17 02:55:49 -0700

    modifying html skeleton

---
+++ intro_app/README.md
@@ -0,0 +1,11 @@
+# Practice HTML application
+## Folder Structure
+
+intro_app
+```
+.
+├── README.md
+├── app.js
+├── index.html
+└── style.css
+```
---
+++ intro_app/app.js
@@ -0,0 +1,8 @@
+console.log("I am connected")
+
+var main = function () {
+    "use strict";
+    window.alert("Hello Teresa!");
+}
+
+$(document).ready(main);
